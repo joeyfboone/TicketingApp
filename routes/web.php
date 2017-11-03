b3772961cd765e2e464e96ed39e4e98cd0ea5d55
joeyfboone <joey@urgentmail.com>
2017-11-03 10:51:28 -0400

    initial setp

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::get('/scan', function () {
+    return view('Scan');
+});
+
+Auth::routes();
+
+Route::get('/scan', 'ScanController@index')->name('scan');
