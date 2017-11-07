50a37dd13a24213f9920da869ad15d7c0240c2a2
joeyfboone <joey@urgentmail.com>
2017-11-07 13:25:42 -0500

    update scanner link

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -33,7 +33,7 @@
                     <div>Status: {{$barcode_status}}</div>
 
                     <a href="RedLaser://">Launch Scanner app (iOS only)</a>
-                    <a href="QR Reader://">Launch Scanner app (iOS only)</a>
+                    <a href="QR$20Reader://">Launch Scanner app (iOS only)</a>
 
                 </div>
             </div>
